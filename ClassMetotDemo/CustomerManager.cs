882ba1737ef0c397f68b47d8d9a81dfe66c19914
eda <edanurozltn@gmail.com>
2021-02-28 02:05:18 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + " " + customer.CustomerSurname + " " + " isimli ve "+ customer.CustomerId + "id numarasına ait müşteriniz ekelnmiştir.");
+        }
+        public void List(Customer[] customers)
+        {
+            foreach (Customer customer in customers)
+            {
+                Console.WriteLine(customer.CustomerName + customer.CustomerSurname);
+            }
+        }
+        public void Delate(Customer customer)
+        {
+            Console.WriteLine(customer.CustomerName + " " + customer.CustomerSurname + " " + " isimli ve " + customer.CustomerId + "id numarasına ait müşteriniz silinmiştir.");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer()
+            { 
+                CustomerName ="Eda Nur",
+                CustomerSurname = "Özaltın",
+                CustomerId = "1001",
+                CustomerAddress = "İstanbul"
+                
+            };
+
+            Customer customer2 = new Customer();
+            customer2.CustomerName = "Fikriye";
+            customer2.CustomerSurname = "Özaltın";
+            customer2.CustomerId = "1002";
+            customer2.CustomerAddress = "İstanbul";
+
+            Customer customer3 = new Customer();
+            customer3.CustomerName = "Murat";
+            customer3.CustomerSurname = "Özaltın";
+            customer3.CustomerId = "1003";
+            customer3.CustomerAddress = "İstanbul";
+
+            Customer[] customers = new Customer[] { customer1, customer2, customer3 };
+
+            CustomerManager customerManager1 = new CustomerManager();
+            customerManager1.Add(customer1);
+
+            CustomerManager customerManager2 = new CustomerManager();
+            customerManager1.Delate(customer2);
+
+            CustomerManager customerManager3 = new CustomerManager();
+            customerManager1.List(customers);
+
+
+
+
+
+
+
+
+        }
+    }
+}
